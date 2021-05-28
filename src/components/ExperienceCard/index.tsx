1c196a6f842eb054ebcdcc549c13fc805b67b6d1
satyowicaksana <satyo.wicaksana@rebelworks.co>
2021-05-28 15:29:11 +0700

    update experience card styling

--- src/components/ExperienceCard/index.tsx
+++ src/components/ExperienceCard/index.tsx
@@ -22,26 +22,21 @@ const ExperienceCard = ({
 
   return (
     <div className='experience-card-container glass p-2'>
-      <Row gutter={16} className='mb-2'>
-        <Col>
-          <Avatar src={experience.src} size='large' shape='square'/>
-        </Col>
-        <Col>
-          <Title level={5}>{experience.title}</Title>
-          <Text>{experience.company} - {experience.type}</Text><br></br>
-          <Text className='typography-fade'>{experience.date}</Text>
-        </Col>
-      </Row>
-      <Paragraph>{experience.description}</Paragraph>
-      <Row gutter={8}>
-        {experience.stacks.map((stack, i) => (
-          <Col key={i} className='mt-2'>
-            <Tooltip overlay={stack.name}>
-              <img src={stack.src} alt='' className='image-icon clickable'/>
-            </Tooltip>
+      <div>
+        <Row gutter={16} className='mb-1'>
+          <Col>
+            <Avatar src={experience.src} shape='square'/>
           </Col>
-        ))}
-      </Row>
+          <Col>
+            <Title level={5}>{experience.title}</Title>
+          </Col>
+        </Row>
+        <Text className='typography-block mb-1'><Link strong>{experience.company}</Link> <Text className='typography-fade'>- {experience.type}</Text></Text>
+        <Paragraph>{experience.description}</Paragraph>
+      </div>
+      <div>
+        <Text strong className='typography-fade'>{experience.date}</Text>
+      </div>
     </div>
   );
 }
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -28,7 +28,7 @@ import { frontEndIllustration } from 'assets/images';
 import Avatar from 'antd/lib/avatar/avatar';
 import { experiences } from './consts';
 
-const { Title, Text } = Typography
+const { Title, Text, Paragraph, Link } = Typography
 const { Step } = Steps
 
 const Home = () => {
@@ -47,7 +47,7 @@ const Home = () => {
                 <Title type='secondary' className='banner-title'>Satyo Wicaksana</Title>
                 <Title type='secondary' className='banner-subtitle typography-fade'>FRONT-END DEVELOPER</Title>
                 <Text type='secondary' className='typography-fade'>based in Jakarta, Indonesia. Specialised in web development using React.</Text>
-                <Button type='primary' size='large' className='mt-4'>Get In Touch 👋</Button>
+                <Link href="https://api.whatsapp.com/send?phone=+628111828395"><Button type='primary' size='large' className='mt-4'>Get In Touch 👋</Button></Link>
               </Col>
               <Col flex='auto'>
                 <img src={frontEndIllustration} alt='' onClick={() => window.open('https://www.freepik.com/vectors/website', '_blank')} className='banner-illustration'/>
@@ -80,7 +80,7 @@ const Home = () => {
             <Title className='mb-4'>Experience & Education</Title>
             <Row align='middle' gutter={16}>
             {experiences.map((experience, i) => (
-              <Col key={i} className='mb-2'>
+              <Col key={i} span={8} className='mb-2'>
                 <ExperienceCard key={i} experience={experience}/>
               </Col>
             ))}
@@ -88,9 +88,40 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className='skills-container centered-flex'>
+      <div className='tools-container centered-flex'>
         <div className='content-container py-5'>
-          <Title>Skills</Title>
+          <Title className='mb-4'>Tools</Title>
+        </div>
+      </div>
+      <div className='portfolio-container centered-flex'>
+        <div className='content-container py-5'>
+          <Title className='mb-4'>Portfolio</Title>
+          <Row align='middle' gutter={40} wrap={false} className='mb-5'>
+            <Col span={8}>
+              <img src='https://yt3.ggpht.com/ytc/AAUvwnis_ocUytOqs0O_PLpYGe8emz0StqNfMG7uzINszw=s900-c-k-c0x00ffffff-no-rj' alt='' className='portfolio-image'/>
+            </Col>
+            <Col>
+              <Paragraph>
+                This is a simple website to show what I can do in web development.<br></br>
+                The concept is a redesign of <Link strong href='https://myanimelist.net/' target='_blank'>MyAnimeList</Link>, an anime database and community website.<br></br>
+                I got the design inspiration from other similar site and from dribbble.<br></br>
+                I use <Link strong href='https://jikan.docs.apiary.io/' target='_blank'>Jikan API</Link> for the anime data and add some data with web scraping using NodeJS. 😎<br></br><br></br>
+                As for now, I'm only done with displaying data in <Link>home page</Link>, <Link>search page</Link>, and <Link>detail page</Link>.<br></br>There's a <Link>register page</Link> but the authorization feature isn't done yet. 😅<br></br>
+                For now at least you can play with the search page. 😄<br></br><br></br>
+                I'm planning to continue developing at least for authorization and some CRUD action. 💪
+              </Paragraph>
+            </Col>
+          </Row>
+          <Row align='middle' gutter={20} wrap={false}>
+            <Col span={4}>
+              <img src='https://yt3.ggpht.com/ytc/AAUvwnis_ocUytOqs0O_PLpYGe8emz0StqNfMG7uzINszw=s900-c-k-c0x00ffffff-no-rj' alt='' className='portfolio-image'/>
+            </Col>
+            <Col>
+              <Paragraph>
+                This is my exercise back when I first learn React.<br></br>I just display data from a free API. 😄
+              </Paragraph>
+            </Col>
+          </Row>
           <p></p>
         </div>
       </div>
